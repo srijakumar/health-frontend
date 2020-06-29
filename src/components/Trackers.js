9384f36bc5f18d6816705b46e8245f0199270385
srijakumar <srijakumar7@gmail.com>
2020-06-29 18:39:25 -0400

    Tried to display all trackers - errors/props not working

--- src/components/Trackers.js
+++ src/components/Trackers.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Trackers = (props) => {
-
+console.log(props)
   return (
     <div>
-      Trackers
+      {props.trackers.map(tracker => <li key={tracker.id}>{tracker.name} - {tracker.description}</li>)}
+      //need props - thats the information being passed from container to child components
     </div>
   )
 
