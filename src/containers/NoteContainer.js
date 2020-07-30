c855c3e951668fe073699db699a1aa589d274434
srijakumar <srijakumar7@gmail.com>
2020-07-30 14:24:31 -0400

    Calendar dislay - issue with date and refresh

--- src/containers/NoteContainer.js
+++ src/containers/NoteContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NoteInput from '../components/NoteInput'
 import Notes from '../components/Notes'
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import moment from 'moment';
 import './style.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -10,45 +10,67 @@ const localizer = momentLocalizer(moment);
 
 class NoteContainer extends React.Component {
 
-  constructor() {
-      super();
-      const now = new Date();
-
+  constructor(props) {
+      super(props);
       this.state = {
         calendar_notes: []
       }
     }
 
-    eventsFunc = (props) => {
-      let events = this.props.tracker.notes
-        for (let i = 0; i < events.length; i++) {
-            events[i].content = "X"
-            events[i].date = moment.utc(events[i].date).toDate();
-            events["start"] = events[i].date
-            events["end"] = events[i].date
-          }
-        this.setState({
-          calendar_notes: events
-        })
-    }
+    // this works but not the first time but after refresh and also X is one day behind
+    // componentDidMount() {
+    //     this.props.tracker.notes.map(noteEditted => {
+    //       this.setState({
+    //         calendar_notes: [...this.state.calendar_notes,
+    //         {
+    //             title: "X",
+    //             start: new Date(noteEditted.date),
+    //             end: new Date(noteEditted.date)
+    //         }]
+    //       })
+    //     })
+    //   }
 
-//I set the start and end date as the this.props.tracker.notes.date because i am not allowing my app to end an end date
+    componentDidMount() {
+       let notesArray = []
+      // loop through the notes and add to array
+            this.props.tracker.notes.map(noteEditted => {
 
-  render(){
+                let note = {
+                    title: "X",
+                    start: new Date(noteEditted.date),
+                    end: new Date(noteEditted.date)
+                }
+                notesArray.push(note)
+            })
+            // after the looping is done, then just set the calendar array to the new one
+            this.setState({
+              calendar_notes: notesArray
+             })
+      }
 
-    const { calendar_notes } = this.state
+      BigCalendar = () => (
+        <div style={{height: '500px'}}>
+         <Calendar
+           selectable
+           localizer={localizer}
+           events={this.state.calendar_notes}
+           defaultDate={moment().toDate()}
+          />
+        </div>
+   )
 
+
+  render() {
     return (
-
       <div>
         <NoteInput tracker={this.props.tracker}/> <br/>
         <Notes notes={this.props.tracker && this.props.tracker.notes}/>
         <div className = "app-side-bar">
-          <Calendar
-            events={calendar_notes}
-            defaultView='month'
-            defaultDate={new Date()}
-          />
+          { this.state.calendar_notes ?
+          <this.BigCalendar />
+         : null
+        }
         </div>
       </div>
     )
@@ -58,3 +80,12 @@ class NoteContainer extends React.Component {
 }
 
 export default NoteContainer
+
+
+// <Calendar
+//   Localizer ={localizer}
+//   events={calendar_notes}
+//   startAccessor='start'
+//   endAccessor='end'
+//   defaultDate={moment().toDate()}
+// />
